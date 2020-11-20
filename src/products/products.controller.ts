cda0e805501b0c518a0267e8ecf1d5567cd7f9fa
Christopher Nohall <christopher.nohall@gmail.com>
2020-11-20 15:11:18 +0900

    can get all or one products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 
 import { ProductsService } from './products.service';
 
@@ -19,4 +19,17 @@ export class ProductsController {
         );
         return {id: generateId}
   }
+
+  @Get()
+  getAllProducts(){
+    return this.productsService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string,){
+    return this.productsService.getSingleProduct(prodId);
+  }
 }
+
+
+
