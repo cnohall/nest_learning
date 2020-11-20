39696d4ef7f8a73ddd204e6f7348c6b42b77d410
Christopher Nohall <christopher.nohall@gmail.com>
2020-11-20 15:34:41 +0900

    rest api before connecting to mongoDB

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
 
 import { ProductsService } from './products.service';
 
@@ -29,6 +29,22 @@ export class ProductsController {
   getProduct(@Param('id') prodId: string,){
     return this.productsService.getSingleProduct(prodId);
   }
+  @Patch(':id')
+  updateProduct(
+      @Param('id') prodId: string, 
+      @Body('title') prodTitle: string,
+      @Body('desc') prodDesc: string,
+      @Body('price') prodPrice: number,
+      ){
+        this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+        return null;
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') prodId: string){
+    this.productsService.deleteProduct(prodId);
+    return null;
+  } 
 }
 
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -18,11 +18,39 @@ export class ProductsService {
     }
     
     getSingleProduct(productId: string) {
-        const product = this.products.find((prod) => prod.id == productId);
+        const product = this.findProduct(productId)[0];
+        return {...product};
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        const [product, index] = this.findProduct(productId);
+        const updateProduct = {...product}
+        if (title){
+            updateProduct.title = title;    
+        }
+        if (desc){
+            updateProduct.description = desc;     
+        }
+        if (price){
+            updateProduct.price = price;     
+        }
+        this.products[index] = updateProduct;
+    }
+
+    deleteProduct(productId: string) {
+        const index = this.findProduct(productId)[1];
+        this.products.splice(index, 1);
+    }
+
+    private findProduct(id: string): [Product, number]{
+        const productIndex = this.products.findIndex((prod) => prod.id == id);
+        const product = this.products[productIndex];
         if (!product) {
             throw new NotFoundException('Could not find product');
         }
-        return {...product};
+        return [product, productIndex];
     }
+
+
 }
 
