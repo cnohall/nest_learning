41ef9921217d477ef0a417fcef6c725f5cec10b5
Christopher Nohall <christopher.nohall@gmail.com>
2020-11-20 19:01:10 +0900

    can now post data to Mongo server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ConfigModule.forRoot(), ProductsModule, MongooseModule.forRoot(
+    'mongodb+srv://cnohall:'+process.env.ATLAS_PASSWORD+'@cluster0.59dl8.mongodb.net/Nesting?retryWrites=true&w=majority'
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
